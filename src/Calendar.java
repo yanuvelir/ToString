7822b1b4fea5d45e88c3e4dc975d031d04c175b5
yanuvelir <yanuvelir@gmail.com>
2024-04-08 21:04:13 +0300

    method toString done

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,7 +1,9 @@
 public class Calendar {
 
     public static void main(String[] args){
-
+        Event e = new Event(24, 8, 1991, "Ukrainian independence day");
+        System.out.printf("%s%n",e);
+        System.out.printf("%s%n", e);
 
     }
 }
--- src/Event.java
+++ src/Event.java
@@ -1,8 +1,19 @@
 public class Event {
 
-    int day;
-    int month;
-    int year;
-    String event;
+    private int day;
+    private int month;
+    private int year;
+    private String event;
+
+    public Event(int d, int m, int y, String e){
+        this.day = d;
+        this.month =m;
+        this.year = y;
+        this.event = e;
+
+    }
+    public  String toString(){
+        return String.format("%d/%d/%d - %s", day, month, year, event);
+    }
 
 }
